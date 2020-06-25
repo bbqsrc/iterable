7cfb53a293a1b75e53f143ada1ac3a4ddb03ad16
Brendan Molloy <brendan@bbqsrc.net>
2020-06-25 11:24:16 +0200

    Split immutable into its own namespace for conditional compilation

--- Iterable.cs
+++ Iterable.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Collections.ObjectModel;
 using System.Runtime.CompilerServices;
 
@@ -251,50 +250,7 @@ namespace Iterable
             return output;
         }
         
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ImmutableDictionary<K, V> ToImmutableDict<K, V>(this IEnumerable<(K, V)> enumerable) where K: notnull {
-            var builder = ImmutableDictionary.CreateBuilder<K, V>();
-            
-            foreach (var (key, value) in enumerable) {
-                builder.Add(key, value);
-            }
-
-            return builder.ToImmutable();
-        }
-        
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ImmutableList<T> ToImmutableList<T>(this IEnumerable<T> enumerable) where T: notnull {
-            var builder = ImmutableList.CreateBuilder<T>();
-            
-            foreach (var value in enumerable) {
-                builder.Add(value);
-            }
-
-            return builder.ToImmutable();
-        }
-        
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ImmutableHashSet<T> ToImmutableSet<T>(this IEnumerable<T> enumerable) where T: notnull {
-            var builder = ImmutableHashSet.CreateBuilder<T>();
-            
-            foreach (var value in enumerable) {
-                builder.Add(value);
-            }
-
-            return builder.ToImmutable();
-        }
-        
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ImmutableArray<T> ToImmutableArray<T>(this IEnumerable<T> enumerable) where T: notnull {
-            var builder = ImmutableArray.CreateBuilder<T>();
-            
-            foreach (var value in enumerable) {
-                builder.Add(value);
-            }
-
-            return builder.ToImmutable();
-        }
-
+       
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T? First<T>(this IEnumerable<T> enumerable, Func<T, bool> predicate) where T: class {
             foreach (var item in enumerable) {
---
+++ Iterable/Immutable.cs
@@ -0,0 +1,56 @@
+﻿#if NETCOREAPP3_1
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
+
+namespace Iterable.Immutable
+{
+    public static class Immutable
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ImmutableDictionary<K, V> ToImmutableDict<K, V>(this IEnumerable<(K, V)> enumerable) where K: notnull {
+            var builder = ImmutableDictionary.CreateBuilder<K, V>();
+            
+            foreach (var (key, value) in enumerable) {
+                builder.Add(key, value);
+            }
+
+            return builder.ToImmutable();
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ImmutableList<T> ToImmutableList<T>(this IEnumerable<T> enumerable) where T: notnull {
+            var builder = ImmutableList.CreateBuilder<T>();
+            
+            foreach (var value in enumerable) {
+                builder.Add(value);
+            }
+
+            return builder.ToImmutable();
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ImmutableHashSet<T> ToImmutableSet<T>(this IEnumerable<T> enumerable) where T: notnull {
+            var builder = ImmutableHashSet.CreateBuilder<T>();
+            
+            foreach (var value in enumerable) {
+                builder.Add(value);
+            }
+
+            return builder.ToImmutable();
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ImmutableArray<T> ToImmutableArray<T>(this IEnumerable<T> enumerable) where T: notnull {
+            var builder = ImmutableArray.CreateBuilder<T>();
+            
+            foreach (var value in enumerable) {
+                builder.Add(value);
+            }
+
+            return builder.ToImmutable();
+        }
+
+    }
+}
+#endif
